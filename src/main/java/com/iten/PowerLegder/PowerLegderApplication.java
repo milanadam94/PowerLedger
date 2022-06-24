5268591b32a17fae37768dd570d66ebaa95aa445
unknown <milan.adam94@gmail.com>
2022-06-24 14:15:34 +0200

    Initial version

---
+++ src/main/java/com/iten/PowerLegder/PowerLegderApplication.java
@@ -0,0 +1,14 @@
+package com.iten.PowerLegder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowerLegderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowerLegderApplication.class, args);
+		System.out.println("Start");
+	}
+
+}
